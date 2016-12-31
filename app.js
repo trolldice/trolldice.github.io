22fcf412351f600c7cf581d4d59eefef553ec0b8
trolldice <mogamezs@gmail.com>
2016-12-31 20:44:14 +0200

    wtf

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// Untitled Dice v0.0.8
+// Troll Dice v0.0.1
 
 // Customize these configuration settings:
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 2564,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Untitled Dice',
+  app_name: 'Troll Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  recaptcha_sitekey: '6LcrFBAUAAAAANDKdxakWfjvmNsYshpuW3GKdbXK',  // <----- EDIT ME!
+  redirect_uri: 'https://trolldice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
